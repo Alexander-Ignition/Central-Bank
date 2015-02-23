88f874fbcb50473a767c60549b74800e42ddd026
Александр Игнатьев <izh.sever@gmail.com>
2015-02-23 14:29:04 +0300

    file CBClient

---
+++ Central Bank/Client/CBClient.h
@@ -0,0 +1,14 @@
+//
+//  CBClient.h
+//  Central Bank
+//
+//  Created by Александр Игнатьев on 23.02.15.
+//  Copyright (c) 2015 Alexander Ignition. All rights reserved.
+//
+//  Doc: http://www.cbr.ru/scripts/Root.asp?PrtId=SXML
+
+@import Foundation;
+
+@interface CBClient : NSObject
+
+@end
---
+++ Central Bank/Client/CBClient.m
@@ -0,0 +1,23 @@
+//
+//  CBClient.m
+//  Central Bank
+//
+//  Created by Александр Игнатьев on 23.02.15.
+//  Copyright (c) 2015 Alexander Ignition. All rights reserved.
+//
+
+#import "CBClient.h"
+
+#import <AFNetworking/AFHTTPSessionManager.h>
+
+
+@interface CBClient ()
+@property (strong, nonatomic) AFHTTPSessionManager *sessionManager;
+@end
+
+
+@implementation CBClient
+
+
+
+@end
